c268c56bac4abae0f3fcf50b41c664514900f170
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2026-04-11 17:17:53 +0900

    u

--- Assets/Scripts/Twwya/Sample.cs
+++ Assets/Scripts/Twwya/Sample.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
 
-public class Sample : MonoBehaviour
+namespace Twwya
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Sample : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
